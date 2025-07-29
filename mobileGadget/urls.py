9669dfe722aaf6b2bb29adb5ff3e811938b3f278
hasdajustin <matheusjustinhasda@gmail.com>
2025-07-29 21:58:12 +0600

    include all app urls

--- mobileGadget/urls.py
+++ mobileGadget/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('earphones/', include('earphones.urls')),
+    path('charges/', include('charges.urls')),
+    path('powerbanks/', include('powerbanks.urls')),
+    path('cables/', include('cables.urls')),
+    path('phonecases/', include('phonecases.urls')),
 ]
