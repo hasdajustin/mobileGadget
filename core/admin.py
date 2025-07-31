743ecf030b5bb8f59a8ad4ac22ef35c019c0a977
hasdajustin <matheusjustinhasda@gmail.com>
2025-08-01 02:41:27 +0600

    register phonecases admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AudioGear, Charger
+from .models import AudioGear, Charger, Phonecases
 
 # Register your models here.
 @admin.register(AudioGear)
@@ -9,5 +9,10 @@ class AudioGearAdmin(admin.ModelAdmin):
 
 @admin.register(Charger)
 class ChargerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Phonecases)
+class PhonecasesAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug')
     prepopulated_fields = {'slug': ('title',)}
