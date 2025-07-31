892849a8c97bd6cb0417f130478c136f66a39488
hasdajustin <matheusjustinhasda@gmail.com>
2025-08-01 02:43:19 +0600

    add phonecases model in views for get phonecases obejct and rendering

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .models import AudioGear, Charger
+from .models import AudioGear, Charger, Phonecases
 
 # Create your views here.
 def home(request):
     audio_gears = AudioGear.objects.all()
     chargers = Charger.objects.all()
+    phonecases = Phonecases.objects.all()
     return render(request, 'pages/index.html', {
         'audio_gears': audio_gears,
-        'chargers': chargers
+        'chargers': chargers,
+        'phonecases': phonecases,
     })
